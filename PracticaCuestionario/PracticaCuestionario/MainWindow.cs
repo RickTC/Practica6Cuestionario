3b9d50efbc83edef3ddb93a273f9d0ef74034721
Ricardo Tapia <RickTC@localhost.localdomain>
2013-11-01 10:22:03 -0600

    boton de terminar con if de la preguntas listo. Fin.

--- PracticaCuestionario/PracticaCuestionario/MainWindow.cs
+++ PracticaCuestionario/PracticaCuestionario/MainWindow.cs
@@ -3,6 +3,9 @@ using Gtk;
 
 public partial class MainWindow: Gtk.Window
 {	
+	
+	int puntos=0;
+	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
@@ -14,8 +17,53 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 
-	protected void OnButton1Clicked (object sender, System.EventArgs e)
+	
+	public void preguntas()
 	{
-		throw new System.NotImplementedException ();
+		
+		if (this.radiodf.Active)
+		{
+			puntos++;
+		}
+		
+		if(this.checkportugal.Active)
+		{
+			puntos++;
+		}
+		if(this.checkreino.Active)
+		{
+			puntos++;
+		}
+		if(this.checkmace.Active)
+		{
+			puntos++;
+		}
+		
+		if(this.estados.Text == "31")
+		{
+			puntos++;
+		}
+		
+		if(calendario.GetDate().ToShortDateString() == "07/05/1945")
+		{
+			puntos++;
+		}
+		
+	}
+	
+	
+	protected void OnTerminarClicked (object sender, System.EventArgs e)
+	{
+		preguntas();
+		MessageDialog mensaje = new MessageDialog 
+		(
+			null,
+			DialogFlags.Modal,
+			MessageType.Info,
+			ButtonsType.None,"Codigo: "+ codigo.Text +"\n"+
+							  "Nombre: "+ nombre.Text +"\n"+
+								"Puntos obtenidos: "+puntos
+		);
+		mensaje.Show();
 	}
 }
